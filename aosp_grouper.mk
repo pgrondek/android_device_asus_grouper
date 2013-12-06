f9df521a9e32a4861a336fd8be35f632db958598
nerull7 <github@nerull7.info>
2013-12-06 11:10:05 +0100

    Modifications for proper support Google Play Store

--- aosp_grouper.mk
+++ aosp_grouper.mk
@@ -16,3 +16,11 @@
 $(call inherit-product, device/asus/grouper/full_grouper.mk)
 
 PRODUCT_NAME := aosp_grouper
+PRODUCT_DEVICE := grouper
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4/KRT16S/573038:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4 KRT16S 573038 release-keys"
+
